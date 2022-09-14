f5f9de83c313de21c052d65fa423268f005cb1c4
Miles Wu <slion0725@gmail.com>
2022-09-14 19:06:44 +0800

    upd

--- docker/run.sh
+++ docker/run.sh
@@ -2,7 +2,9 @@
 
 cd /var/www
 
+php artisan package:discover
 php artisan optimize:clear
 php artisan optimize
+php artisan event:cache
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
